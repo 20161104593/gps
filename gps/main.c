887e515005db9402dd787f990a8230f9b33ff245
20161104593 <a20161104593@King.local>
2017-06-16 18:07:50 +0800

    123

--- gps/main.c
+++ gps/main.c
@@ -5,11 +5,24 @@
 //  Created by 20161104593 on 17/6/16.
 //  Copyright © 2017年 20161104593. All rights reserved.
 //
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main()
+{
+    FILE *fp;
+    FILE *fr;
+    char a[100]={0};
+    int i=0;
+    fp=fopen("//Users//a20161104593//Desktop//gps//1.txt","r");
+    fr=fopen("//Users//a20161104593//Desktop//gps//2.txt","w");
+    for(i=0;i<1;i++)
+    {
+        fscanf(fp,"%s\n",&a[i]);
+    }
+    for(i=0;i<1;i++)
+    {
+        fprintf(fr,"%s\n",&a[i]);
+        printf("%s",&a[i]);
+    }
+    fclose(fr);
     return 0;
 }
